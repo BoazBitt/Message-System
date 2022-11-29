ce16a222845cb818c6e49e108b3ed84510d8d065
Boaz Bitton <boaz2119@gmail.com>
2022-11-29 03:07:31 +0200

    settings update

--- message_app/settings.py
+++ message_app/settings.py
@@ -35,8 +35,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))  # Instead of your actual secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# 'localhost', 'Boaz2119.pythonanywhere.com'
+ALLOWED_HOSTS = ['localhost', 'Boaz2119.pythonanywhere.com']
+#
 
 
 # Application definition
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
